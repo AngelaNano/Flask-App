521726540044100aadc26303938d0133d5095553
AngelaNano <109978629+AngelaNano@users.noreply.github.com>
2026-02-13 00:24:10 -0500

    FlaskApp1.py

--- FlaskApp1.py
+++ FlaskApp1.py
@@ -1,4 +1,4 @@
 import requests
 BASE_URL = "http://127.0.0.1:5000"
-print(requests.get(f"{BASE_URL}/hello").json())
-print(requests.get(f"{BASE_URL}/data").json())
+print(requests.get(f"{BASE_URL}/api/hello").json())
+print(requests.get(f"{BASE_URL}/api/data").json())
